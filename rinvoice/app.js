36dd4a0e7a45576fe85b314314f3eea6939a6a90
Hiranya <hiranyagarbhchoudhary@gmail.com>
2021-03-07 12:42:21 +0530

    update cookie setting

--- rinvoice/app.js
+++ rinvoice/app.js
@@ -47,10 +47,16 @@ global.db = conn; //single global DB connection
 
 // ====== [ session ] ======
 app.use(session({
-  secret: 'keyboard cat',
+  secret: 'keyboard cat', // in production - store in env variable
   resave: false,
   saveUninitialized: true,
-  cookie: { maxAge: 60000 }
+  cookie: { 
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    // secure: true, == for production
+    secure: false,
+    sameSite: 'strict',
+  }
 }));
 
 
