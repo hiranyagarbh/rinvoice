8c9660483b0924027f537d656ea05c7a5c08c544
Hiranya <hiranyagarbhchoudhary@gmail.com>
2021-03-09 19:37:41 +0530

    add invoice upload feature

--- rinvoice/routes/invoices.js
+++ rinvoice/routes/invoices.js
@@ -1,6 +1,4 @@
-
-// ==== ======= [ invoices after login ] ======= ====
-
+// ==== ======= [ invoices view route ] ======= ====
 exports.invoices = function (req, res, next) {
 
     var user = req.session.user;
@@ -13,5 +11,30 @@ exports.invoices = function (req, res, next) {
     var sql = "SELECT * FROM users WHERE id = ?";
     db.query(sql, [userId], function (err, results) {
         res.render('pages/home/invoices.ejs', { data: results });
+    });
+};
+
+// ==== ======= [ add invoice route ] ======= ==== 
+exports.addInvoice = function (req, res) {
+    var message = '';
+    var userId = req.session.userId;
+    var addinv = req.body;
+    var service = addinv.service;
+    var amount = addinv.amount;
+    var buyer_email = addinv.buyer_email;
+    // get uploaded file path
+    var in_filePath = req.file.path;
+    // urgent checkbox
+    var checkurgent = 'n';
+    if (addinv.checkurgent == 'on') { checkurgent = 'y'; }
+
+    var sql = "INSERT INTO invoices VALUES (?,?,?,?,?)";
+    db.query(sql, [service, amount, buyer_email, in_filePath, checkurgent], function(err, results){
+        if (err) {
+            console.log(err.message);
+        } else {
+            console.log('Successfully uploaded!');
+            res.redirect('/home/invoices');
+        }
     });
 };
