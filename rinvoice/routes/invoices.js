af00cb66a037bcec0317f1ba23207cc9b7147775
Hiranya <hiranyagarbhchoudhary@gmail.com>
2021-03-13 15:15:13 +0530

    invoice action btns functs

--- rinvoice/routes/invoices.js
+++ rinvoice/routes/invoices.js
@@ -7,34 +7,35 @@ exports.invoices = function (req, res, next) {
     if (userId == null) {
         res.redirect("/login");
         // return;
+    } else {
+        var get_acctype = 'SELECT acctype FROM users WHERE id = ?'; // get account type from userId
+        db.query(get_acctype, [userId], function (err, results) {
+
+            var res_acctype = JSON.parse(JSON.stringify(results))[0].acctype;
+
+            // check if account type is merchant or buyer
+            // left join handles the case - if the returned results arr is empty
+            if (res_acctype == 'm') {
+                var get_data = 'SELECT name, acctype, service, amount, buyeremail, invoiceId, filepath, urgent, status FROM users LEFT JOIN invoices ON users.id = invoices.m_uid WHERE id = ? ORDER BY dateadded desc;';
+                db.query(get_data, [userId], function (err, results) {
+                    // add error handler here
+                    console.log(results);
+                    res.render('pages/home/invoices.ejs', { data: results });
+                });
+
+            } else if (results[0].acctype == 'b') {
+                var get_data = 'SELECT name, acctype, m_uid, m_email, service, amount, invoiceId, filepath, urgent, status FROM users LEFT JOIN invoices ON users.email = invoices.buyeremail WHERE id = ? ORDER BY dateadded desc;';
+                db.query(get_data, [userId], function (err, results) {
+                    // add error handler here
+                    // console.log(results);
+                    res.render('pages/home/invoices.ejs', { data: results });
+                });
+
+            } // elseif
+
+        }); // db.query
     }
 
-    var get_acctype = 'SELECT acctype FROM users WHERE id = ?'; // get account type from userId
-    db.query(get_acctype, [userId], function (err, results) {
-
-        var res_acctype = JSON.parse(JSON.stringify(results))[0].acctype;
-        
-        // check if account type is merchant or buyer
-        // left join handles the case - if the returned results arr is empty
-        if (res_acctype == 'm') {
-            var get_data = 'SELECT name, acctype, service, amount, buyeremail, filepath, urgent, status FROM users LEFT JOIN invoices ON users.id = invoices.m_uid WHERE id = ? ORDER BY dateadded desc;';
-            db.query(get_data, [userId], function (err, results) {
-                // add error handler here
-                // console.log(results);
-                res.render('pages/home/invoices.ejs', { data: results });
-            });
-
-        } else if (results[0].acctype == 'b') {
-            var get_data = 'SELECT name, acctype, m_uid, m_email, service, amount, filepath, urgent, status FROM users LEFT JOIN invoices ON users.email = invoices.buyeremail WHERE id = ? ORDER BY dateadded desc;';
-            db.query(get_data, [userId], function (err, results) {
-                // add error handler here
-                // console.log(results);
-                res.render('pages/home/invoices.ejs', { data: results });
-            });
-
-        } // elseif
-
-    }); // db.query
 
 };
 
@@ -48,7 +49,7 @@ exports.addInvoice = function (req, res) {
     var service = addinv.service;
     var amount = addinv.amount;
     var buyer_email = addinv.buyer_email;
-
+    var invoiceId = Math.floor(Math.random() * 1000000);
     // get uploaded file path
     var in_filePath = req.file.path;
 
@@ -59,14 +60,41 @@ exports.addInvoice = function (req, res) {
     // invoice status (initial char) - [ nill (default value), approved, rejected ]
     var invstatus = 'n';
 
-    var sql = "INSERT INTO invoices (m_uid, m_email, service, amount, buyeremail, filepath, urgent, status) VALUES (?,?,?,?,?,?,?,?)";
-    db.query(sql, [userId, m_email, service, amount, buyer_email, in_filePath, checkurgent, invstatus], function (err, results) {
-        if (err) {
-            // add GUI err handler
-            console.log(err.message);
-        } else {
-            console.log('Successfully uploaded!'); // remove in production
-            res.redirect('/home/invoices');
-        }
+    var sql = "INSERT INTO invoices (m_uid, m_email, service, amount, buyeremail, invoiceId, filepath, urgent, status) VALUES (?,?,?,?,?,?,?,?,?)";
+    db.query(sql, [userId, m_email, service, amount, buyer_email, invoiceId, in_filePath, checkurgent, invstatus], function (err, results) {
+        if (err) { console.log(err.message); }
+        res.redirect('/home/invoices');
     });
+};
+
+// ==== ======= [ invoice action bts ] ======= ==== 
+
+// view funct
+exports.invActionView = function (req, res) {
+    // console.log('view invoice');
+};
+
+// approve funct
+exports.invActionApprove = function (req, res) {
+    var invoiceId = req.body.apprinv;
+    var approve_query = "UPDATE invoices SET status = 'a' WHERE invoiceId = ?";
+    db.query(approve_query, [invoiceId], function (err) { if (err) { console.log(err.message); } });
+    res.redirect('/home/invoices');
+};
+
+// reject funct
+exports.invActionReject = function (req, res) {
+    var invoiceId = req.body.rejeinv;
+    var reject_query = "UPDATE invoices SET status = 'r' WHERE invoiceId = ?";
+    db.query(reject_query, [invoiceId], function (err) { if (err) { console.log(err.message); } });
+    res.redirect('/home/invoices');
+};
+
+// delete funct
+// this funct does not delete the file yet -- add file deletion
+exports.invActionDelete = function (req, res) {
+    var invoiceId = req.body.deleinv;
+    var delete_query = "DELETE FROM invoices WHERE invoiceId = ?";
+    db.query(delete_query, [invoiceId], function (err) { if (err) { console.log(err.message); } });
+    res.redirect('/home/invoices');
 };
